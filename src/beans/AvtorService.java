5ff9b86f9f1424fdc49b3296b579da9a922caa76
Yevhen <eugeny.berkunsky@gmail.com>
2016-02-03 18:21:18 +0200

    03.02 Added filter by comment + edited jsp + glassfish-web.xml (for russian) + html

--- src/beans/AvtorService.java
+++ src/beans/AvtorService.java
@@ -5,7 +5,6 @@ import tables.Avtor;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import java.util.List;
 
 /**
